8786ae7c117e4384dfc331e5ec75e675920a4e19
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2026-07-07 23:10:39 +0530

    Add project source files

---
+++ src/component/Card.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// const card = (props) => { 
+//     console.log()
+//   return (
+//     <div>
+//       <div className='card'>
+//         <img src={props.img} alt="img" />
+//         <h1>{props.user},{props.age}</h1>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore doloribus officia sint maiores quod magnam tenetur voluptas sed et quasi eius voluptatum perferendis, eos ab nesciunt eaque dolor maxime!
+//       <br />
+//       <button>view</button>
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default card
