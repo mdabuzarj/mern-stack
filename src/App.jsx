2b8b1f067542db8d148e9ba64d343c3f3a39ef8f
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2026-07-13 11:46:20 +0530

    learn useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+// useState :
 const App = () => {
+  const [num, setNum] = useState(0)
+   function increaseNum(){
+    setNum(num+1);
 
+   }
+   function decreasingNum(){
+    setNum(num-1);
 
+   }
+   function increase5Num(){
+    setNum(num+5);
+    
+
+   }
   return (
-    <div className='box' onMouseMove={val}>
-      hello
+    <div>
+    <h1>{num}</h1>
+    <button onClick={increaseNum}>increase</button>
+    <button onClick={decreasingNum} >decrease</button>
+    <button onClick={increase5Num}>increaseby5</button>
     </div>
   )
   }
